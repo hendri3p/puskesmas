af40541813e541a7251e8d61cb6531d1f7fdd4ae
a123 <a123@TBRG-Telagas-Mac.local>
2022-07-12 00:23:57 +0700

    update

--- application/models/M_berobat.php
+++ application/models/M_berobat.php
@@ -1,12 +1,11 @@
 <?php 
  
-class M_datapasien extends CI_Model{	
+class M_berobat extends CI_Model{	
 	function tampil_data(){
 		return $this->db->get('berobat');
 	}
 	
 	function input_data($data,$table){
 		$this->db->insert($table,$data);
-	}
-	
+	}	
 }
